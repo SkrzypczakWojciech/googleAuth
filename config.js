812fe39ba84fd24933c79225e0a79f989eb7862c
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-08-09 23:42:56 +0200

    init project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID' : '990909785195-tqavn9nfpe07h7flcrookqiq2u172hcd.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET' : 'ey2WueUJ_tgETNl7e005r4Zv',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback',
+};
